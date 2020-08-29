d87c8cb77d6387dcba8445fe00d282c2268c4b6f
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-08-30 00:44:12 +0530

    [Vaibhav]ADDED: feature of start with fresh board

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -1,16 +1,14 @@
 console.log('*******Welcome To Tic-Tac-Toe Game*******');
-let board: Array<number> = new Array<number>();
-
-for (let position = 1; position <= 9; position++) {
-    board[position] = position;
-}
-
+let board = new Array();
+//for (let position = 1; position <= 9; position++) {
+//    board[position] = position;
+//}
 let displayBoard = () => {
     console.log("\t\t  " + board[1] + " | " + board[2] + " | " + board[3] + " ");
     console.log("\t\t-------------");
     console.log("\t\t  " + board[4] + " | " + board[5] + " | " + board[6] + " ");
     console.log("\t\t-------------");
     console.log("\t\t  " + board[7] + " | " + board[8] + " | " + board[9] + " ");
-}
-
+};
 displayBoard();
+//# sourceMappingURL=app.js.map
